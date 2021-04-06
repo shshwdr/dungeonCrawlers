cebb0aa65ed4a1a33402b594c7faf2ae83d9bc3b
shshwdr <cs.vivian04@gmail.com>
2021-04-05 19:25:27 -0500

    add boss

---
+++ Assets/DeathTrigger.cs
@@ -0,0 +1,28 @@
+using PixelCrushers.DialogueSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+
+    public GameObject disableItem;
+    public string dialogueVariable;
+    private void OnDestroy()
+    {
+        if(disableItem)
+        disableItem.SetActive(false);
+        DialogueLua.SetVariable(dialogueVariable, true);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
